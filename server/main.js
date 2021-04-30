7954318c18dbc3b8f25b7e252c42c44f15d8ef03
vedransehovic <vedran.sehovic@gmail.com>
2021-04-30 12:18:17 -0400

    Bays collection created

--- server/main.js
+++ server/main.js
@@ -1,32 +1,40 @@
 //importing Meteor
 import { Meteor } from 'meteor/meteor';
 import { LinksCollection } from '/imports/api/links';
+import { BaysCollection } from '../imports/api/bays';
+
 
 function insertLink({ title, url }) {
   LinksCollection.insert({title, url, createdAt: new Date()});
 }
 
+function insertBay({ name, jobName, sort }) {
+  BaysCollection.insert({name, jobName, sort, createdAt: new Date()});
+}
+
 Meteor.startup(() => {
   // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
+  if (BaysCollection.find().count() === 0) {
+    insertBay({
+      name: "Bay 1",
+      jobName: '',
+      sort: '1'
+    });
+    insertBay({
+      name: "Bay 2",
+      jobName: '',
+      sort: '2'
+    });
+    insertBay({
+      name: "Bay 3",
+      jobName: '',
+      sort: '3'
+    });
+    insertBay({
+      name: "Bay 4",
+      jobName: '',
+      sort: '4'
     });
 
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
   }
 });
