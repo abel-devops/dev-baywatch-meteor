81135ab8e5cafa175f3a94ac61fecc45b137f444
Vedran Sehovic <63619895+vedransehovic@users.noreply.github.com>
2021-06-09 14:38:27 -0400

    edit bay form created

---
+++ imports/ui/components/EditBay.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const editBay = () => {
+  return (
+    <div>
+      <h1>Edit Bay:</h1>
+      <form>
+        <label>
+          Production:
+          <input type="text" name="production" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+};
+
+export default editBay;
